1434b868df77f434d09a7565cb679be0f17406d9
skandalabrandur <steinerinn@gmail.com>
2019-03-11 21:46:14 +0000

    whoo boy

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,12 +4,19 @@ class Post < ApplicationRecord
     validate :image_presence
     validates :title, presence: true
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validate :image_type
 
     private
 
     def image_presence
         unless image.attached?
             errors.add(:image, "Vantar mynd!")
+        end
+    end
+
+    def image_type
+        if !image.content_type.in?(%('image/jpeg image/png'))
+            errors.add(:image, 'Mynd verður að vera JPEG/JPG eða PNG')
         end
     end
 end
