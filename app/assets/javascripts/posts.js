f29fff5abbe8cf01a592a7d03c04f3f313068acf
skandalabrandur <steinerinn@gmail.com>
2019-03-14 23:56:14 +0000

    atrocities

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,12 +1,24 @@
 function previewImage(input) { 
   if (input.files && input.files[0]) { 
+    console.log(input.files[0]);
     var reader = new FileReader(); 
  
     reader.onload = function (e) { 
-      $('#prev-img') 
+      var extension = input.files[0].name.substring(input.files[0].name.lastIndexOf('.') + 1);
+      console.log(extension);
+      if((input.files[0].size / (1000*1000)) > 50.0) {
+        $('#submitter').hide();
+        alert("Hámarksstærð leyfileg er 50MB. Þín mynd er:" + (input.files[0].size / (1000*1000)) + "MB");
+      } else if(extension != "jpg" && extension != "jpeg" && extension != "png") {
+        $('#submitter').hide();
+        alert('Mynd þarf að vera .jpg, .jpeg eða .png');
+      } else {
+        $('#submitter').show();
+        $('#prev-img') 
         .attr('src', e.target.result) 
         //.width(150) 
         .height(200); 
+      }
     }; 
  
     reader.readAsDataURL(input.files[0]); 
